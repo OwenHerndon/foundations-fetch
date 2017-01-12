e8dc8183992c4c4c7f48e9f0d23d3a1a86dcdf50
Owen <owen.w.herndon@gmail.com>
2017-01-11 19:02:51 -0600

    added drop down pass in url and selected method

--- mainController.js
+++ mainController.js
@@ -1,12 +1,17 @@
 ;(function () {
   angular.module('fetchApp').controller('MainController', function ($scope, requestService, Response) {
+    
+    $scope.methods = ["Get", "Post", "Options", "Head"];
+
     $scope.responses = [];
     $scope.url = "http://httpstat.us/200";
 
-    $scope.fetch = () => {
+    $scope.fetch = (url, selectedMethod) => {
       requestService.get($scope.url).then((success) => {
         const response = new Response(success);
         $scope.responses.push(response);
+        $scope.urlFetched = url;
+        $scope.selectedMethodFetched = selectedMethod;
       }, (error) => {
         debugger
         //do something else
